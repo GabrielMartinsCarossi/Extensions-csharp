f15816095acd332bedc71c350407b340423ed330
Gabriel Martins Carossi <gabrielmcarossi@gmail.com>
2021-07-31 16:35:01 -0300

    Programa com extension methods de string e DateTime

---
+++ Course/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2021, 07, 31, 16, 27, 00);
+            Console.WriteLine(dt.ElapsedTime());//Extended method for DateTime
+
+            string texto = "This text is way too long, it might need to be cut.";
+            Console.WriteLine(texto.Cut(30));//Extended method for string
+           
+        }
+    }
+}
